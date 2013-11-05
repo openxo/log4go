2365d92ec9702b4a795837f3f80e4f0f218dbe12
Samuel Lindholm <samuel.lindholm@gmail.com>
2013-11-05 10:23:30 +0000

    set examples to use forked log4go

--- examples/FileLogWriter_Manual.go
+++ examples/FileLogWriter_Manual.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-import l4g "code.google.com/p/log4go"
+import l4g import l4g "github.com/openxo/log4go"
 
 const (
 	filename = "flw.log"
--- examples/XMLConfigurationExample.go
+++ examples/XMLConfigurationExample.go
@@ -1,6 +1,6 @@
 package main
 
-import l4g "code.google.com/p/log4go"
+import l4g import l4g "github.com/openxo/log4go"
 
 func main() {
 	// Load the configuration (isn't this easy?)
